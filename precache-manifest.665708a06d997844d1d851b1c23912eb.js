7e76c9be389032cea5fe98cfa0e663ebdb3878a9
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2019-11-06 22:48:37 -0300

    Updates

---
+++ precache-manifest.665708a06d997844d1d851b1c23912eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7280e1cd1de4ae159452c9b8c9150ceb",
+    "url": "/meme-generator-react/index.html"
+  },
+  {
+    "revision": "3d0e67a6973d7630c1e8",
+    "url": "/meme-generator-react/static/css/main.c903be81.chunk.css"
+  },
+  {
+    "revision": "4eedf63af06f92d46bf0",
+    "url": "/meme-generator-react/static/js/2.72e4dc18.chunk.js"
+  },
+  {
+    "revision": "3d0e67a6973d7630c1e8",
+    "url": "/meme-generator-react/static/js/main.fc85651b.chunk.js"
+  },
+  {
+    "revision": "65bb50fb134de72b5a12",
+    "url": "/meme-generator-react/static/js/runtime-main.3fc5ab11.js"
+  }
+]);
